c32d62cf777d2f99bbfbb64144847fbdf529575e
saforer <saforer@gmail.com>
2013-11-30 06:41:14 -0500

    Wooo weapons

--- Assets/Scripts/Prefab/Gun.cs
+++ Assets/Scripts/Prefab/Gun.cs
@@ -5,7 +5,8 @@ public class Gun : MonoBehaviour {
 
 	public Rigidbody2D Ammunition;
 	public Transform GunObject;
-
+    public int Multi;
+    public bool split;
 	// Use this for initialization
 	void Start () {
 
@@ -17,8 +18,10 @@ public class Gun : MonoBehaviour {
 	}
 
 	public void Shoot() {
+ 
 		Rigidbody2D bulletInstance = Instantiate(Ammunition, GunObject.position, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
         bulletInstance.GetComponent<Projectile>().SetFacingRight(GetComponent<Movement>().facingRight);
         bulletInstance.GetComponent<Projectile>().SetOwnerTag(gameObject.tag);
-	}
+
+    }
 }
--- Assets/Scripts/Prefab/Projectile.cs
+++ Assets/Scripts/Prefab/Projectile.cs
@@ -7,9 +7,17 @@ public class Projectile : MonoBehaviour {
     private bool _FacingRight;
     int Direction;
     public int Damage;
+    public bool bouncable;
+    public int bounces;
+    public bool connected;
+    private Transform player;
 	// Use this for initialization
 	void Start () {
         Direction = (_FacingRight) ? 1 : -1;
+        if(connected) {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            GetComponent<SpringJoint2D>().connectedBody = player.GetComponent<Rigidbody2D>();
+        }
         rigidbody2D.velocity = transform.right*projectilespeed*Direction;
 	}
 	
@@ -33,9 +41,30 @@ public class Projectile : MonoBehaviour {
                 Destroy (gameObject);
             }
         }
-        if(col.collider.tag == "Ground") {
-            Destroy (gameObject);
+        if(col.gameObject.CompareTag("Projectile")) {
+            if(!bouncable) {
+                Destroy (gameObject);
+            } else {
+                bounces--;
+                if (bounces>=0) {
+                    rigidbody2D.velocity = new Vector2(Random.Range(-20,20),Random.Range (-40,40));
+                } else {
+                    Destroy (gameObject);
+                }
+            }
         }
+        if(col.gameObject.CompareTag("Ground")) {
+                if(!bouncable) {
+                    Destroy (gameObject);
+                } else {
+                    bounces--;
+                    if (bounces>=0) {
+                        rigidbody2D.velocity = new Vector2(Random.Range(-20,20),Random.Range (-40,40));
+                    } else {
+                        Destroy (gameObject);
+                    }
+                }
+            }
     }
 
     public void SetFacingRight (bool FacingRight) {
